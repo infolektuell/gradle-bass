5b58bffb0d3d596715a9f2c18c494ddea682aa85
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-09-21 20:56:37 +0200

    Update example to use gradle native

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,9 +1,18 @@
+import de.infolektuell.gradle.bass.tasks.ExtractBass
+
 plugins {
-    id("base")
+    `cpp-application`
     id("de.infolektuell.bass") version "0.1.0"
 }
 
 bass.libraries {
-    register("bassflac")
     register("bassmix")
+}
+val extractTask: ExtractBass = tasks.withType(ExtractBass::class).first()
+
+application {
+    dependencies {
+        implementation(fileTree(extractTask.natives.dir("lib")))
+    }
+    privateHeaders.from(extractTask.natives.dir("include"))
 }
