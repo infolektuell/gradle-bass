fc35c4bf0af76688a6a232b18e9ba4a131f44a78
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-09-21 16:30:35 +0200

    Simplify example project to be more minimalistic

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,27 +1,9 @@
-import de.infolektuell.gradle.bass.tasks.ExtractBass
 plugins {
-    id("application")
-    id("de.infolektuell.jextract") version "0.1.0"
-    id("de.infolektuell.bass")
+    id("base")
+    id("de.infolektuell.bass") version "0.1.0"
 }
 
 bass.libraries {
-    // register("bassflac")
+    register("bassflac")
     register("bassmix")
-    register("basswv")
-}
-
-jextract.libraries {
-    create("bass") {
-        header = tasks.withType(ExtractBass::class).first().headers.file("bass.h")
-        targetPackage = "com.un4seen.bass"
-        headerClassName = "Bass"
-        libraries = listOf("bass")
-    }
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(22)
-    }
 }
