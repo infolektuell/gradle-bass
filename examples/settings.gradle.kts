ebc20d929c1f8c99415dadd1719459e8217490b7
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-09-22 10:39:08 +0200

    Convert example project to multi-module examples project

--- examples/settings.gradle.kts
+++ examples/settings.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "example"
+rootProject.name = "examples"
+include("cpp-application")
 includeBuild("../")
