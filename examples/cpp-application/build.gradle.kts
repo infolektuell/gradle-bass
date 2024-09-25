add7d47c363eb8c81d5daf44e167d17942dd76be
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2024-09-25 19:46:25 +0200

    Cpp-application example: Configure link and runtime deps separately

--- examples/cpp-application/build.gradle.kts
+++ examples/cpp-application/build.gradle.kts
@@ -7,9 +7,12 @@ bass.libraries {
     register("bassmix")
 }
 
+val linkFiles: FileTree = fileTree(bass.layout.lib).matching { include("*.dylib", "*.so", "*.lib") }
+val runtimeFiles: FileTree = fileTree(bass.layout.lib).matching { include("*.dylib", "*.so", "*.dll") }
 application {
-    dependencies {
-        implementation(fileTree(bass.layout.lib))
-    }
     privateHeaders.from(bass.layout.include)
+    binaries.configureEach(CppExecutable::class) {
+        linkTask.get().libs.from(linkFiles)
+        installTask.get().libs = runtimeFiles
+    }
 }
